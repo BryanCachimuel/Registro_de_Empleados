6706846884229b03768c613c5c64fd372ef15b85
BryanCachimuel <bryanloyo56@gmail.com>
2022-10-30 21:01:52 -0500

    corrigiendo errores de redireccionamiento en cada funcionalidad creada dentro del controller

--- controller/empleados.controller.php
+++ controller/empleados.controller.php
@@ -2,8 +2,8 @@
 
 include("../config/conexion.php");
 
-// utilizaremos los valores ternarios para decir que si encuentra el
-// valor lo almacene en la variable txtId y caso contrario se pondrá vacio
+/*utilizaremos los valores ternarios para decir que si encuentra el
+  valor lo almacene en la variable txtId y caso contrario se pondrá vacio*/
 $txtId=(isset($_POST['txtId']))?$_POST['txtId']:"";
 $txtNombre=(isset($_POST['txtNombre']))?$_POST['txtNombre']:"";
 $txtApellidoP=(isset($_POST['txtApellidoP']))?$_POST['txtApellidoP']:"";
@@ -13,7 +13,7 @@ $txtFoto=(isset($_FILES['txtFoto']["name"]))?$_FILES['txtFoto']["name"]:"";
 
 $accion=(isset($_POST['accion']))?$_POST['accion']:"";
 
-// este dato va a servir para guardar todos los errores
+/*este dato va a servir para guardar todos los errores*/
 $error=array();
 
 /* se crean estas acciones para poder controlar el modal cuando se trata solo 
@@ -59,11 +59,11 @@ switch ($accion) {
         $nombreArchivo=($txtFoto!="")?$fecha->getTimestamp()."_".$_FILES["txtFoto"]["name"]:"imagen.jpg";
         $tmpFoto=$_FILES["txtFoto"]["tmp_name"];
         if($tmpFoto!=""){
-            move_uploaded_file($tmpFoto,"../imagenes/".$nombreArchivo);
+            move_uploaded_file($tmpFoto,"../public/images/".$nombreArchivo);
         }
         $query->bindParam(':foto',$nombreArchivo);
         $query->execute();
-        header("Location:index.php");
+        header("Location: ../views/empleados.php");
 
     break;
 
@@ -82,10 +82,10 @@ switch ($accion) {
         $nombreArchivo=($txtFoto!="")?$fecha->getTimestamp()."_".$_FILES["txtFoto"]["name"]:"imagen.jpg";
         $tmpFoto=$_FILES["txtFoto"]["tmp_name"];
         if($tmpFoto!=""){
-            move_uploaded_file($tmpFoto,"../imagenes/".$nombreArchivo);
+            move_uploaded_file($tmpFoto,"../public/images/".$nombreArchivo);
                 
-           // mediante este código se modifica la imagen ya que si se tiene una imagen guardada anteriormenete
-           // y se sube otra esta se cambia 
+           /*mediante este código se modifica la imagen ya que si se tiene una imagen guardada anteriormenete
+             y se sube otra esta se cambia */
             $query=$pdo->prepare("SELECT foto FROM empleados WHERE id=:id");
             $query->bindParam(':id',$txtId);
             $query->execute();
@@ -94,17 +94,17 @@ switch ($accion) {
             if(isset($empleado["foto"])){
                 if(file_exists("../imagenes/".$empleado["foto"])){
                    if($item['foto']!="imagen-jpg"){
-                     unlink("../imagenes/".$empleado["foto"]);
+                     unlink("../public/images/".$empleado["foto"]);
                    }
                 }
             }
-            // seccion solo para actualizar el campo foto
+            /*seccion solo para actualizar el campo foto*/
             $query=$pdo->prepare("UPDATE empleados SET foto=:foto WHERE id=:id");
             $query->bindParam(':foto',$nombreArchivo);
             $query->bindParam(':id',$txtId);
             $query->execute();
         }
-        header("Location:index.php");
+        header("Location: ../views/empleados.php");
 
     break;
 
@@ -113,13 +113,13 @@ switch ($accion) {
         $query->bindParam(':id',$txtId);
         $query->execute();
 
-        // PDO::FETCH_LAZY -> solo nos va a devolver un dato de la tabla seleccionada 
-        // file_exist -> verifica si existe un archivo 
-        // unlink -> borra de la carpeta la imagen si existe
+        /*PDO::FETCH_LAZY -> solo nos va a devolver un dato de la tabla seleccionada 
+          file_exist -> verifica si existe un archivo 
+          unlink -> borra de la carpeta la imagen si existe*/
         $empleado=$query->fetch(PDO::FETCH_LAZY);
         if(isset($empleado["foto"]) && ($item['foto']!="imagen.jpg")){
-            if(file_exists("../imagenes/".$empleado["foto"])){
-                unlink("../imagenes/".$empleado["foto"]);
+            if(file_exists("../public/images/".$empleado["foto"])){
+                unlink("../public/images/".$empleado["foto"]);
             }
         }
         
@@ -127,7 +127,7 @@ switch ($accion) {
         $query->bindParam(':id',$txtId);
         $query->execute();
    
-        header("Location:index.php");
+        header("Location: ../views/empleados.php");
         
     break;
 
@@ -149,16 +149,14 @@ switch ($accion) {
     break;
 
     case "btnCancelar":
-        header("Location:index.php");
+        header("Location: ../views/empleados.php");
     break;
 }
-// se va a ejecutar la consulta sql con esto $query->execute();
-// seguido $query se va almacenar en la variable $listaEmpleados con la cual se va a obtener la información 
-// mediante PDO::FETCH_ASSOC que es lo que hace es devolver o asociar información de la base de datos a un arreglo
+/*se va a ejecutar la consulta sql con esto $query->execute();
+  seguido $query se va almacenar en la variable $listaEmpleados con la cual se va a obtener la información 
+  mediante PDO::FETCH_ASSOC que es lo que hace es devolver o asociar información de la base de datos a un arreglo*/
 $query=$pdo->prepare("SELECT * FROM empleados WHERE 1");
 $query->execute();
 $listaEmpleados=$query->fetchAll(PDO::FETCH_ASSOC);
-
-
 
 ?>
