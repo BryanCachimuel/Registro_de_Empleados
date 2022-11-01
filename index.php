3e15145149b3f78e52881b04122b860200e1e098
BryanCachimuel <bryanloyo56@gmail.com>
2022-10-31 22:23:23 -0500

    maquetando con html5 la vista principal del proyecto

--- index.php
+++ index.php
@@ -4,12 +4,48 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registro de Empleados</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+    <title>Menú con portada - MagtimusPro</title>
+
+    <link rel="stylesheet" href="./public/css/estilos.css">
+    <script src="https://kit.fontawesome.com/41bcea2ae3.js" crossorigin="anonymous"></script>
+
 </head>
-<body>
-    <a class="btn btn-primary" href="./views/empleados.php">Vista Empleados</a>
+<body>   
+
+    <header id="header">
+        <div class="container__header">
+            <div class="logo">
+                <img src="./public/images/logo-magtimus.png">
+            </div>
+            <div class="container__nav">
+                <nav id="nav">
+                    <ul>
+                        <li><a href="./views/empleados.php" class="select">Iniciar Sesión</a></li>
+                    </ul>
+                </nav>         
+                <div class="btn__menu" id="btn_menu"><i class="fas fa-bars"></i></div>
+            </div>
+        </div>
+    </header>
+
+    <div class="container__all" id="container_all">
+
+        <div class="cover">
+
+            <div class="container__cover">
+
+                <div class="container__info">
+                    <h1>Registro de</h1>
+                    <h2>Empleados</h2>
+                    <p>Sistema creado para el registro de empleados de la empresa</p>
+                    <input type="button" value="Registro">
+                </div>
+                <div class="container__vector">
+                    <img src="./public/images/undraw_Code_thinking_re_gka2.svg">
+                </div>
+            </div>
+
+        </div>
+<script src="js/script.js"></script>
 </body>
 </html>
