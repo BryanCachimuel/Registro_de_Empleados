c3338307750845db4a2ea4d22f4e2195ee809b3f
BryanCachimuel <bryanloyo56@gmail.com>
2023-06-10 21:59:20 -0500

    creando las funciones para mostrar las contraseñas en los formularios para inicio de sesión y registro de usuarios

---
+++ public/js/mostrar.js
@@ -0,0 +1,20 @@
+const mostrarContrasenia = () => {
+    let mostrar = document.getElementById('inputPassword');
+    if(mostrar.type === "password"){
+        mostrar.type = "text";
+    }else{
+        mostrar.type = "password";
+    }
+}
+
+const compararContrasenia = () => {
+    let mostrar = document.getElementById('inputPassword');
+    let confirmarcontrasenia = document.getElementById('inputConfirmPassword');
+    if(mostrar.type === "password" && confirmarcontrasenia.type === "password"){
+        mostrar.type = "text";
+        confirmarcontrasenia.type = "text";
+    }else{
+        mostrar.type = "password";
+        confirmarcontrasenia.type = "password"; 
+    }
+}
